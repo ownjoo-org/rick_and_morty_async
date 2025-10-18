51722cc5b9d1d52074648be69a7ca4cb80e1ec39
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2025-10-18 14:56:54 -0400

    async is all working; TODO: figure out how to stop waiting for the Queue()

--- rick_and_morty_async/parser.py
+++ rick_and_morty_async/parser.py
@@ -2,10 +2,14 @@ from asyncio import Queue
 from json import dumps
 
 
-async def parse(q: Queue):
+async def parse(q: Queue, subscriber_count: int = 1) -> None:
+    should_continue: bool = True
     endl = ''
     print('[')
-    while result := await q.get():
+    while subscriber_count > 0:
+        result = await q.get()
+        if result is None:
+            subscriber_count -= 1
         print(f'{endl}{dumps(result, indent=4)}', end='')
         endl = ',\n'
     print(']')
